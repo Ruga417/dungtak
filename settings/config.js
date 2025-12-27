3fb0e06f42c31e73dcd8df8cd80a66ed58473ee8
Ruga417 <rugasukamatcha@gmail.com>
2025-12-27 10:34:16 +0700

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -3,6 +3,13 @@ const fs = require("fs");
 global.thumbnail = "https://g.top4top.io/p_3635g6w6v1.jpg"
 global.ownerUsername = "Caynnmac"
 global.ownerId = "7691095744"
+global.domain = "https://caynnpanel-site.jkt48.my.id"
+global.plta = "ptla_jyB6sAVWX6Fy4Pf9tjPwgtw4vpLGm4dR7oVlAWjAh9Q"
+global.pltc = "ptlc_W9AZbpqkGepdSuFwA0JD28uExw8YeditJWXadFr0eCn"
+global.loc = "1"
+global.eggs = "15"
+global.nests = "5"
+
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
   fs.unwatchFile(file);
